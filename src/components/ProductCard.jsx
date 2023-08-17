0efdde992d082f6072f512caeddcd1d758370681
l3g4cy-0 <legacyclashps@gmail.com>
2023-08-17 11:42:12 +0530

    Home backend added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import CardImage from "../assets/img.png";
 import "../styles/ProductCard.css";
 
-const ProductCard = (props) => {
+const ProductCard = ({props}) => {
   console.log(props)
   return (
     <div className="productcard">
       <div className="productDetails">
         <img src={props.imgUrl} className="cardImage" />
         <div className="title">{props.name}</div>
-        <div className="priceStriked">$499. 00</div>
+        <div className="priceStriked">{props.price}</div>
         <div className="productPrice">$499.00</div>
       </div>
       <button>Add to Cart</button>
